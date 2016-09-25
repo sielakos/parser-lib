cc3d3c4fa314fdce11c21d4ffb672f63ebeb727d
sielakos <sielakos@gmail.com>
2016-09-25 21:40:34 +0200

    <improvement>(symbol): implement symbol using State.consumeText

--- lib/blocks/symbol.ts
+++ lib/blocks/symbol.ts
@@ -4,12 +4,7 @@ import {startsWith} from '../utils';
 export function symbol(symbol: string): Parser<any, string> {
   return createParser((state: State<any>) => {
     if (startsWith(state.str, symbol)) {
-      const newState = new State(
-        state.str.substr(symbol.length),
-        symbol,
-        state.row,
-        state.col + symbol.length
-      );
+      const newState = state.consumeText(symbol);
 
       return Either.point(newState);
     }
